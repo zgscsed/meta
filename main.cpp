e94a2c7db06fe143025d8ce9a6be09ef4d122dd8
zgscsed <1106659535@qq.com>
2024-01-31 23:19:07 +0800

    模板初始--函数模板

---
+++ main.cpp
@@ -0,0 +1,7 @@
+﻿#include <iostream>
+
+int main(void)
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
---
+++ testTemplate.hpp
@@ -0,0 +1,11 @@
+#ifndef TESTTEMPLATE_HPP_
+#define TESTTEMPLATE_HPP_
+
+// 加法模板函数
+template <typename T>
+T add(T a, T b)
+{
+    return a + b;
+}
+
+#endif /* TESTTEMPLATE_HPP_ */
